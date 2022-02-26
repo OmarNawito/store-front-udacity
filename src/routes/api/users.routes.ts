740f66b3a9fe65bec918b4416cd531f69e1f410a
omarNawito <o_mohsen@hotmail.com>
2022-02-26 15:29:03 +0200

    add routes

--- src/routes/api/users.routes.ts
+++ src/routes/api/users.routes.ts
@@ -3,5 +3,5 @@ import { Router } from 'express'
 
 const routes = Router()
 // api/users
-
+routes.route('/').get(() => console.log('users'))
 export default routes
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import routes from './routes'
 
 const app: express.Application = express()
 const PORT: number =  process.env.PORT as unknown as number || 3020;
 const address: string = `0.0.0.0:${PORT}`
 
 app.use(bodyParser.json())
-
+app.use('/api', routes)
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
-
 app.use(function (req, res) {
     res.status(404);
     res.send("Page not found");
